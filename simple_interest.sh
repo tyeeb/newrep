7665bccf6df5a56ccc5fbd6bb92a997bcb3cba34
Opbecy <79321708+tyeeb@users.noreply.github.com>
2022-10-09 18:06:11 +0530

    Create simple_interest.sh

---
+++ simple_interest.sh
@@ -0,0 +1,8 @@
+
+p = int(input('The principal is: '))
+t = int(input('The time period is: '))
+r = int(input('The rate of interest is: '))
+	
+si = (p * t * r)/100
+	
+print('The Simple Interest is', si)
